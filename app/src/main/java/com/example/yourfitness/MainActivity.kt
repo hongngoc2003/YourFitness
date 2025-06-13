dbe5b42c6dfe6bb92fdc24f4f9d165a958d72909
hongngoc2003 <nhngoc1506@gmail.com>
2025-06-14 01:55:52 +0700

    open gallery

--- app/src/main/java/com/example/yourfitness/MainActivity.kt
+++ app/src/main/java/com/example/yourfitness/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.example.yourfitness
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.net.Uri
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
     private lateinit var galleryBtn: Button
     private lateinit var cameraBtn: Button
+
+    //TODO get the image from gallery and display it
+    var galleryActivityResultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(), ActivityResultCallback {
+            if (it.resultCode === RESULT_OK) {
+                val image_uri: Uri? = it.data?.data
+                imageView.setImageURI(image_uri)
+            }
+        }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +43,12 @@ class MainActivity : AppCompatActivity() {
         imageView = findViewById(R.id.imageView)
         galleryBtn = findViewById(R.id.button)
         cameraBtn = findViewById(R.id.button2)
+
+        //TODO chose image from gallery
+        galleryBtn.setOnClickListener {
+            val galleryIntent =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            galleryActivityResultLauncher.launch(galleryIntent)
+        }
     }
 }
