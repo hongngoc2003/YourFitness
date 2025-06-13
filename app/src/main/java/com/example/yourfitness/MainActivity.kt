2f7bf1de6e5673b17028a21cae6f7eda0d2b90c3
hongngoc2003 <nhngoc1506@gmail.com>
2025-06-14 01:08:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/yourfitness/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.yourfitness
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var galleryBtn: Button
+    private lateinit var cameraBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        imageView = findViewById(R.id.imageView)
+        galleryBtn = findViewById(R.id.button)
+        cameraBtn = findViewById(R.id.button2)
+    }
+}
